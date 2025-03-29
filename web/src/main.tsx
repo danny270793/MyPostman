58785ab55e6a4def4111dd64432071713bb95579
Danny Vaca <danny270793@icloud.com>
2025-03-28 21:09:50 -0500

    if is codova wait untill is ready

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function renderReactDom() {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom()
+  }, false);
+} else {
+  renderReactDom()
+}
