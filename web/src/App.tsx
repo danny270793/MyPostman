92779fb1a33252b09a030f06f1ad64bd8631d94c
Danny Vaca <danny270793@icloud.com>
2025-03-28 23:58:51 -0500

    base scaffold

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,40 @@
+import { ChangeEvent, ChangeEventHandler, FormEvent, FormEventHandler, MouseEventHandler, useState } from 'react';
+import './styles/w3css.css'
+
 function App() {
+  const methods: string[] = [
+    'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'
+  ]
+
+  const [method, setMethod] = useState<string>('GET')
+  const onMethodChange: ChangeEventHandler<HTMLSelectElement> = (event: ChangeEvent<HTMLSelectElement>) => {
+    setMethod(event.target.value)
+  }
+
+  const [url, setUrl] = useState<string>('')
+  const onUrlChange: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value)
+  }
+
+  const onFormSubmit: FormEventHandler<HTMLFormElement> = (event: FormEvent) => {
+    event.preventDefault()
+
+    alert(`${method} ${url}`)
+  }
+
   return (
     <>
-      <input type="checkbox" checked={window.cordova !== undefined}/>
-      <label>Cordova</label>
+      <form onSubmit={onFormSubmit}>
+        <select className="w3-select w3-border" value={method} onChange={onMethodChange}>
+          {methods.map(method => <option value={method} key={method}>
+            {method}
+          </option>)}
+        </select>
+        <input className="w3-input w3-border" type="text" placeholder="URL" value={url} onChange={onUrlChange}/>
+        <button className='w3-button w3-blue'>
+          Send
+        </button>
+      </form>
     </>
   )
 }
