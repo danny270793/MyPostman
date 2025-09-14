dfb069f4606dc46cc68ec0f84f03201ef2f1f01d
Danny Vaca <danny270793@icloud.com>
2025-09-13 20:22:01 -0500

    feat: add components barrel export for clean imports
    - Export all major components: Header, Sidebar, RequestBuilder, ResponseViewer, LoadingScreen
    - Enable clean imports: import { Header, Sidebar } from '../components'
    - Centralized component access point for better maintainability
    - Single import line for multiple components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Component exports for easy importing
+export { Header } from './Header'
+export { Sidebar } from './Sidebar'
+export { RequestBuilder } from './RequestBuilder'
+export { ResponseViewer } from './ResponseViewer'
+export { LoadingScreen } from './LoadingScreen'
